c702e92d918f832190a7abc719d97bfe22253bc0
Gaagul <gaagul@admins-MacBook-Pro.local>
2022-08-25 18:27:21 +0530

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module Granite
+  class Application < Rails::Application
+    # previous code if any
+    config.load_defaults 6.1
+
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
+  end
+end
