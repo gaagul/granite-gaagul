2533adb70fb766503627149a1113664eada6f686
Gaagul <gaagul@admins-MacBook-Pro.local>
2022-09-01 16:35:42 +0530

    Added signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def index
-    users = User.select(:id, :name)
-    render status: :ok, json: { users: users }
+  # previous code
+
+  def create
+    user = User.new(user_params)
+    user.save!
+    respond_with_success(t("successfully_created", entity: "User"))
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
