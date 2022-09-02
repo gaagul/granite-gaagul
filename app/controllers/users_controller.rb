d79034c2174773a9da682dea743e518e5d776b95
Gaagul <gaagul@admins-MacBook-Pro.local>
2022-09-02 12:11:48 +0530

    Added login feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  # previous code
+  skip_before_action :authenticate_user_using_x_auth_token, only: :create
+
+  def index
+    users = User.select(:id, :name)
+    render status: :ok, json: { users: users }
+  end
 
   def create
     user = User.new(user_params)
