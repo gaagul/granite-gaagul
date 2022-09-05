4cf5209647bedcd16d8e249b7cf9343e1b993d3e
Gaagul <gaagul@admins-MacBook-Pro.local>
2022-09-05 15:54:27 +0530

    Added starring feature to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@
 class Task < ApplicationRecord
   RESTRICTED_ATTRIBUTES = %i[title task_owner_id assigned_user_id]
 
+  enum status: { unstarred: "unstarred", starred: "starred" }
+
   enum progress: { pending: "pending", completed: "completed" }
 
   has_many :comments, dependent: :destroy
@@ -18,6 +20,17 @@ class Task < ApplicationRecord
   before_create :set_slug
 
   private
+
+    def self.of_status(progress)
+      if progress == :pending
+        starred = pending.starred.order("updated_at DESC")
+        unstarred = pending.unstarred.order("updated_at DESC")
+      else
+        starred = completed.starred.order("updated_at DESC")
+        unstarred = completed.unstarred.order("updated_at DESC")
+      end
+      starred + unstarred
+    end
 
     def set_slug
       itr = 1
