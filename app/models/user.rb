952c2a92aa86e558eebd070d6cbfacdf00e78c21
Gaagul <gaagul@admins-MacBook-Pro.local>
2022-09-05 10:10:04 +0530

    Added tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  MAX_NAME_LENGTH = 35
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
 
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -10,7 +11,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :authentication_token
 
-  validates :name, presence: true, length: { maximum: 35 }
+  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
     length: { maximum: MAX_EMAIL_LENGTH },
