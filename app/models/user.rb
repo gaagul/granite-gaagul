90c3f4a08ebe6002f1fb7cdeced2a4abf128bdea
Gaagul <gaagul@admins-MacBook-Pro.local>
2022-09-05 16:03:44 +0530

    Fixed order of items in Active Record models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,14 @@
 
 class User < ApplicationRecord
   MAX_NAME_LENGTH = 35
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  MAX_EMAIL_LENGTH = 255
+
   has_many :comments, dependent: :destroy
 
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
 
-  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  MAX_EMAIL_LENGTH = 255
-
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
-  has_secure_password
-  has_secure_token :authentication_token
 
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email, presence: true,
@@ -23,6 +21,9 @@ class User < ApplicationRecord
 
   before_save :to_lowercase
   before_destroy :assign_tasks_to_task_owners
+
+  has_secure_password
+  has_secure_token :authentication_token
 
   private
 
